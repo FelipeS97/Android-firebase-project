d052bf4eedd048a192b7c51472fb2d02a14055dc
Felipe Santos <felipecosta97@gmail.com>
2022-07-04 23:07:34 -0300

    tela de login

---
+++ app/src/main/java/br/com/androidfirebaseproject/Activity/Login.java
@@ -0,0 +1,16 @@
+package br.com.androidfirebaseproject.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.androidfirebaseproject.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
