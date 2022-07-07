79ae2372543993d1dcbd2d66ca72092bfa3ce4f1
Felipe Santos <felipecosta97@gmail.com>
2022-07-07 19:17:16 -0300

    tela de login funcional 02

--- app/src/main/java/br/com/androidfirebaseproject/Activity/Login.java
+++ app/src/main/java/br/com/androidfirebaseproject/Activity/Login.java
@@ -6,9 +6,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
@@ -50,24 +53,38 @@ public class Login extends AppCompatActivity {
                 String loginEmail = edt_email.getText().toString();
                 String loginSenha = edt_senha.getText().toString();
 
-                if (!TextUtils.isEmpty(loginEmail) || !TextUtils.isEmpty(loginSenha)){
+                if (!TextUtils.isEmpty(loginEmail) || !TextUtils.isEmpty(loginSenha)) {
                     loginProgressBar.setVisibility(View.VISIBLE);
-                    mAuth.signInWithEmailAndPassword(loginEmail,loginSenha)
+                    mAuth.signInWithEmailAndPassword(loginEmail, loginSenha)
                             .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                                 @Override
                                 public void onComplete(@NonNull Task<AuthResult> task) {
-                                    if(task.isSuccessful()) {
+                                    if (task.isSuccessful()) {
                                         abrirTelaPrincipal();
-                                    }else{String error = task.getException().getMessage();
+                                    } else {
+                                        String error = task.getException().getMessage();
                                         Toast.makeText(Login.this, "", Toast.LENGTH_SHORT).show();
                                         loginProgressBar.setVisibility(View.VISIBLE);
                                     }
                                 }
                             });
+                }
             }
         });
+
+        ckb_mostrar_senha.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    edt_senha.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                }else{
+                    edt_senha.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                    }
+                }
+
+        });
     }
-}
+
 
     private void abrirTelaPrincipal() {
         Intent intent = new Intent(Login.this, MainActivity.class);
--- app/src/main/java/br/com/androidfirebaseproject/MainActivity.java
+++ app/src/main/java/br/com/androidfirebaseproject/MainActivity.java
@@ -4,25 +4,45 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import br.com.androidfirebaseproject.Activity.Login;
 
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    private Button btn_logout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        btn_logout = findViewById(R.id.btn_logout);
+
+        btn_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 
     @Override
     protected void onStart(){
         super.onStart();
-        FirebaseAuth currentuser = FirebaseAuth.getInstance();
+        FirebaseUser currentuser = FirebaseAuth.getInstance().getCurrentUser();
         if(currentuser == null){
             Intent intent = new Intent(MainActivity.this, Login.class);
             startActivity(intent);
